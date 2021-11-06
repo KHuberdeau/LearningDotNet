ffe624dde4de42aa77e6e5c13fce8e4b981a8c7e
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-06 17:47:22 -0600

    Started and Completed LearningDictionaries

---
+++ LearningDictionaries/Dictionaries.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningDictionaries
+{
+    class Dictionaries
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String, String> config = new Dictionary<string, string>();
+
+            config.Add("resolution", "1920x1080");
+            config.Add("title", "MyWebsite");
+
+            Console.WriteLine(config["title"]);
+
+            foreach (var setting in config)
+            {
+                Console.WriteLine(setting.Value);
+            }
+        }
+    }
+}
