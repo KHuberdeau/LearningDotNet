2a29c8a7f564ed25cf7e34445b2ab8f85b949c75
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 16:52:51 -0700

    Finished string interpolation

--- LearningStringInterpolation/Interpolation.cs
+++ LearningStringInterpolation/Interpolation.cs
@@ -8,7 +8,7 @@ namespace LearningStringInterpolation
         {
             var point = new { x = 1.1234, y = 2.1254 };
 
-            Console.WriteLine($"x {point.x} y {point.y}");
+            Console.WriteLine($"x {point.x} y {point.y,10:N1:}");
         }
     }
 }
