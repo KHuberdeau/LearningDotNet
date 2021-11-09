c32362b9e6e8d9084ec07bf34e515e5c87a8397e
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 15:36:50 -0700

    Started and finished Tuple

---
+++ LearningTuple/Tuples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LearningTuple
+{
+    class Tuples
+    {
+        static void Main(string[] args)
+        {
+            //Tuple<int, String, bool> myTuple = new Tuple<int, string, bool>(1, "hello", true);
+
+            var myTuple = Tuple.Create(1, "hello", true);
+
+            Console.WriteLine(myTuple.Item2);
+        }
+    }
+}
