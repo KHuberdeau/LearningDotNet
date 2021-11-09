c486558c263500a3a4cd4a444dd43162ad14c08b
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 16:50:25 -0700

    Finished string interpolation

---
+++ LearningStringInterpolation/Interpolation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LearningStringInterpolation
+{
+    class Interpolation
+    {
+        static void Main(string[] args)
+        {
+            var point = new { x = 1, y = 2 };
+
+            Console.WriteLine("x " + point.x + " y " + point.y);
+
+            Console.WriteLine("x {0} y {1}", point.x, point.y);
+
+            Console.WriteLine($"x {point.x} y {point.y}");
+        }
+    }
+}
