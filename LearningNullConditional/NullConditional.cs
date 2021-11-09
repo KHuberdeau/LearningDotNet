b4d40e21bd51963deaa52f356b0e599ac68bf7eb
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 16:42:31 -0700

    Started Null Conditional

---
+++ LearningNullConditional/NullConditional.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LearningNullConditional
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+    class NullConditional
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person() { Name = "Tim" }; //what if this was null
+            Console.WriteLine(person.Name);
+        }
+    }
+}
