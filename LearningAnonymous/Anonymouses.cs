0f45942e3edb7271c42f46a02a8cd21daf7fa250
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 16:07:19 -0700

    Started, copied the start of Anonymouses

---
+++ LearningAnonymous/Anonymouses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAnonymous
+{
+    class Anonymouses
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("downloading file");
+            Download();
+            Console.ReadLine();
+
+        }
+
+        static void Download()
+        {
+            Network.Download((message) => Console.WriteLine("Download complete " + message));
+        }
+    }
+
+    // Imaginary external network library
+
+    class Network
+    {
+        static public void Download(Action<String> callback)
+        {
+            Task.Run(() => {
+                Thread.Sleep(3000);
+                callback("completed");
+            });
+        }
+    }
+}
+}
