8b49e0152cf42f9f853e72073a13e5de8ed4010e
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 16:38:03 -0700

    Finished Threads

--- LearningThreads/Threads.cs
+++ LearningThreads/Threads.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LearningThreads
 {
@@ -6,7 +8,26 @@ namespace LearningThreads
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 50; i++)
+            {
+                Thread mythread = new Thread(new ThreadStart(Work));
+                mythread.Start();
+
+                Task.Run(() =>
+                {
+                    Console.WriteLine("starting task in thread: " + Thread.CurrentThread.ManagedThreadId);
+                    Thread.Sleep(3000);
+                    Console.WriteLine("task complete");
+                });
+            }
+            Console.ReadLine();
+        }
+
+        static void Work()
+        {
+            Console.WriteLine("starting in thread: " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(3000);
+            Console.WriteLine("work complete");
         }
     }
 }
