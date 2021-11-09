61907156aca95b6a7beed91205e0d09ece86506e
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 16:15:38 -0700

    Copied start of Properties

---
+++ LearningProperties/Property.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LearningProperties
+{
+    class Property
+    {
+        class Player
+        {
+            public bool isAlive = true;
+            int health = 100;
+
+            public void Hit()
+            {
+                Random r = new Random();
+                health -= r.Next(5, 50);
+            }
+
+            public void CheckDeath()
+            {
+                if (health <= 0)
+                    isAlive = false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            for (int i = 0; i < 20; i++)
+            {
+                player.Hit();
+                player.CheckDeath();
+                Console.WriteLine("Is player alive: " + player.isAlive);
+            }
+        }
+    }
+}
