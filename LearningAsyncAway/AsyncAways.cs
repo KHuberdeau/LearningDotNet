8173c62c2b3d6ee969fea66e69e0fec8ceeadb17
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 16:00:47 -0700

    Started, copied the start of Async Away

---
+++ LearningAsyncAway/AsyncAways.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAsyncAwayStart
+{
+    class AsyncAways
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("downloading file");
+            Download();
+            Console.ReadLine();
+
+        }
+
+        static void Download()
+        {
+            Network.Download();
+        }
+    }
+
+
+    // Imaginary external network library
+
+    class Network
+    {
+        static public Task Download()
+        {
+            return Task.Run(() => Thread.Sleep(3000));
+        }
+    }
+}
