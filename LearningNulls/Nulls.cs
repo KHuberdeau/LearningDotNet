d47f11be93385d69f93b1a94bf1e7a562b6cf929
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 16:38:41 -0700

    Started Nulls

---
+++ LearningNulls/Nulls.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningNulls
+{
+    class Nulls
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
