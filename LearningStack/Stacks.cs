55573ec161a448a321260f1dfc3c125c46c697c1
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 15:42:50 -0700

    Started and Finished Stack

---
+++ LearningStack/Stacks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningStack
+{
+    class Stacks
+    {
+        static void Main(string[] args)
+        {
+            Stack<String> pancakes = new Stack<string>();
+
+            pancakes.Push("First pancake made");
+            pancakes.Push("Second pancake made");
+            pancakes.Push("Last pancake made");
+
+            //foreach (var pancake in pancakes)
+            //{
+            //    Console.WriteLine(pancakes);
+            //}
+
+            Console.WriteLine(pancakes.Pop());
+            Console.WriteLine(pancakes.Peek());
+            Console.WriteLine(pancakes.Peek());
+        }
+    }
+}
