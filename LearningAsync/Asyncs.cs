07cbe297500e05e6fc3b2e990cefeef923726f07
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 15:51:07 -0700

    Started Async

---
+++ LearningAsync/Asyncs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningAsync
+{
+    class Asyncs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
