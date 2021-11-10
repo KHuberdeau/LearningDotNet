cae2dc5c790ebbc095909a84793a298fb27263e1
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 17:19:06 -0700

    Finished Exceptions

--- LearningExceptions/Exceptions.cs
+++ LearningExceptions/Exceptions.cs
@@ -1,17 +1,46 @@
 ﻿using System;
+using System.Text;
 
 namespace LearningExceptions
 {
+    class Person
+    {
+        private String name;
+
+        public String Name
+        {
+            get { return name; }
+            set 
+            {
+                try
+                {
+                    if (value == null)
+                        throw new NullReferenceException("name must have a value");
+                    name = value;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("name cannot be null");
+                    throw;
+                }
+            }
+        }
+
+    }
     class Exceptions
     {
         static void Main(string[] args)
         {
             try
             {
-                string s = null;
-                var l = s.Length;
+                var person = new Person();
+                person.Name = null;
             }
-            catch (Exception ex)
+            catch (EncoderFallbackException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (NullReferenceException ex)
             {
                 Console.WriteLine(ex);
             }
