f7a1cec97c9939e65fb0b3218de31e44d00ff8dd
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 17:32:12 -0700

    Started, and finished using

---
+++ LearningUsing/Usings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LearningUsing
+{
+    class Usings
+    {
+        class MyResource : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("Disposing our resource");
+            }
+        }
+        static void Main(string[] args)
+        {
+            using(var resource = new MyResource())
+            {
+                Console.WriteLine("Using my resource");
+            }
+            Console.WriteLine("Finished");
+        }
+    }
+}
