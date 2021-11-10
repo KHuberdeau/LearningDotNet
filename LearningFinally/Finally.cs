e59df6f946db0c2df825475bd7634bc1a0b7060b
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 17:23:54 -0700

    Started and Finished finally

---
+++ LearningFinally/Finally.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LearningFinally
+{
+    class Finally
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connected to database");
+
+            try
+            {
+                throw new Exception("error while accessing database resource");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("exception caught");
+            }
+            finally
+            {
+                Console.WriteLine("disconnecting database");
+            }
+        }
+    }
+}
