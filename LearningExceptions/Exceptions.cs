7066e182f7ba3a70c6f60f3ebb0e6f899edbcf4d
KHuberdeau <59401861+KHuberdeau@users.noreply.github.com>
2021-11-09 17:11:32 -0700

    Started Exceptions

---
+++ LearningExceptions/Exceptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LearningExceptions
+{
+    class Exceptions
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string s = null;
+                var l = s.Length;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
